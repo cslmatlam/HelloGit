c22402b085ad2ff80df5461fe28377b8def133b1
Arcotect <Arcotect@10.171.34.23>
2011-09-07 10:15:06 +0800

    add

--- src/csl/HelloWorld.java
+++ src/csl/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World.");
 		System.out.println("Hello World.");
+		System.out.println("Hello World.");
 	}
 
 }
